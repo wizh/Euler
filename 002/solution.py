bf07b8221b4723260cdcfa09b7a692243d83fcc5
Mathias Fleig Mortensen <contact@mathiasmortensen.dk>
2015-11-19 17:27:01 +0100

    Avoid shadowing reserved keyword

--- 002/solution.py
+++ 002/solution.py
@@ -1,7 +1,7 @@
-sum, a, b = 0, 0, 1
+res, a, b = 0, 0, 1
 
-while sum <= 4000000:
+while res <= 4000000:
     a, b = b, a + b
     if b % 2 == 0:
-        sum += b
-print(sum)
+        res += b
+print(res)
