604e8776b984ddf00669d9c29e232b6ef164d28e
Mathias Fleig Mortensen <contact@mathiasmortensen.dk>
2016-10-10 23:43:36 +0200

    indentation fix

--- 004/solution.py
+++ 004/solution.py
@@ -1,3 +1,3 @@
 # Don't worry, I now switched to 2.7
 print(max(i * j for i in range(900, 1000)
-                    for j in range(900, 1000) if str(i*j) == str(i*j)[::-1]))
+                for j in range(900, 1000) if str(i*j) == str(i*j)[::-1]))
